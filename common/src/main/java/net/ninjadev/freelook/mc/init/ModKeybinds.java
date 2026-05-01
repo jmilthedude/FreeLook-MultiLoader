2c263704fa8bff2c494d2b314162e7446b65d44e
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2026-05-01 17:08:04 -0500

    update to 1.21.11

--- common/src/main/java/net/ninjadev/freelook/mc/init/ModKeybinds.java
+++ common/src/main/java/net/ninjadev/freelook/mc/init/ModKeybinds.java
@@ -1,12 +1,12 @@
 package net.ninjadev.freelook.mc.init;
 
 import net.minecraft.client.KeyMapping;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.apache.commons.lang3.ArrayUtils;
 import org.lwjgl.glfw.GLFW;
 
 public class ModKeybinds {
-    public static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(ResourceLocation.parse("freelook:category"));
+    public static final KeyMapping.Category CATEGORY = KeyMapping.Category.register(Identifier.parse("freelook:category"));
     public static KeyMapping keyFreeLook = new KeyMapping("freelook.key.use", GLFW.GLFW_KEY_LEFT_ALT, CATEGORY);
     public static KeyMapping keyToggleMode = new KeyMapping("freelook.key.toggle", GLFW.GLFW_KEY_RIGHT_ALT, CATEGORY);
 
